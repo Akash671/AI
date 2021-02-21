e03c0ae42a5e855d750c686ac176de09ede60866
Akash Kumar <65735040+Akash671@users.noreply.github.com>
2021-02-21 14:27:05 +0530

    Update chat_app1.py

--- chat_app1.py
+++ chat_app1.py
@@ -1,5 +1,12 @@
-#    author akash kumar
-
+"""
+@author.    : AKASH KUMAR
+@institute. : MIT Institute Moradabad India
+@branch.    : Computer Science & Engineering
+@work as.   : Software Devloper & Machine Learning Engineer
+@website.   : https://medium.com/@akashsaininasa
+@github.    : https://github.com/Akash671
+@LinkedIn.  : https://www.linkedin.com/in/akash-kumar-52563018a
+"""
 
 from chatterbot import ChatBot
 chatbot = ChatBot("Ron Obvious")
